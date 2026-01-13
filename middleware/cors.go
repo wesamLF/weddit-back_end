e9eb30e1f583f23e91c94404dd1c18fa2a92470b
wesam lafi <wesamlafi390@gmail.com>
2026-01-14 00:30:12 +0300

    updated the CORS in the /middleware/cors.go from localhost to my Vercel frontend

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,7 +6,7 @@ import (
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://weddit-front-end.vercel.app")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
