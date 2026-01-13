cf957f18a297cedc3d78a8bd52f12faa8de37dae
wesam lafi <wesamlafi390@gmail.com>
2026-01-13 23:58:08 +0300

    updated the mongodb url in initDB.go

--- db/initDB.go
+++ db/initDB.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -25,12 +24,7 @@ var CommentsCOLname = "comments"
 
 func init() {
 
-	var err = godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	mongoURL := os.Getenv("MONGODB_URL")
-	fmt.Print("mongoURL", mongoURL)
 	clientOpts := options.Client().ApplyURI(mongoURL)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
