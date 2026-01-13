f0240379f0b7ccc688cfec6090ef151af2ff48ab
wesam lafi <wesamlafi390@gmail.com>
2026-01-14 00:26:34 +0300

    updated the CORS in the /middleware/cors.go from localhost to my Vercel frontend

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,20 +6,10 @@ import (
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-
-		// Allowed origins
-		allowedOrigins := map[string]bool{
-			"http://localhost:5173": true,
-			"https://weddit-front-end-git-main-wesamlfs-projects.vercel.app": true,
-		}
-
-		if allowedOrigins[origin] {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Credentials", "true") // allow cookies
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		}
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
 		// Handle preflight OPTIONS request
 		if r.Method == http.MethodOptions {
